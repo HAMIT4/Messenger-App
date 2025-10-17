f9766841bcbabdd5f4243e185292263544f4c59a
HAMIT4 <hamit.ht4@gmail.com>
2025-10-17 13:14:23 +0300

    completed chat

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -20,11 +20,32 @@ const Chat = () => {
           <View style={styles.chatContainer}>
               <Image source={item.photos} style={styles.image} />
               <View style={styles.chatContent}>
-                <View></View>
+                <View style={styles.chatContent}>
+                    <View style={styles.chatHeader}>
+                      <Text style={styles.chatName}>{item.name}</Text>
+                      <Text style={styles.chatTime}>{item.time}</Text>
+                    </View>
+                    <View style={styles.messages}>
+                      <Text style={styles.chatMessages} numberOfLines={1}>
+                        {item.lastMessage}
+                      </Text>
+                      {item.totalUnread > 0 && (
+                        <View style={styles.unreadContainer}>
+                          <Text style={styles.totalUnread}>{item.totalUnread}</Text>
+                        </View>
+                      )}
+                    </View>
+                </View>
               </View>
           </View>
         )}
       />
+      <TouchableOpacity
+        style={styles.chatButton}
+        onPress={() =>{'#'}}
+      >
+        <MaterialCommunityIcons name='android-messages' size={24} color='white' />
+      </TouchableOpacity>
     </View>
   )
 }
@@ -46,6 +67,61 @@ const styles = StyleSheet.create({
     height: 50,
     backgroundColor: '#128C7E',
     borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  chatContent:{
+    flex: 5,
+    borderBottomWidth: 0,
+    marginLeft: 8,
+    paddingBottom: 10,
+    marginBottom: 5,
+  },
+  chatHeader:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 2,
+    marginTop: 4,
+  },
+  chatName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  chatTime:{
+    fontSize:12,
+    color: '#A0A09e'
+  },
+  messages:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  chatMessages:{
+    fontSize:14,
+    color: '#A0A09E',
+    width: '90%'
+  },
+  unreadContainer:{
+    height: 20,
+    width: 25,
+    borderRadius: 40,
+    backgroundColor: '#25D366',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  totalUnread:{
+    fontSize: 10,
+    color: '#fff',
+    fontWeight: '600',
+  },
+  chatButton:{
+    position:'absolute',
+    bottom:70,
+    right: 20,
+    backgroundColor: '#0e806a',
+    width: 60,
+    height:60,
+    borderRadius: 30,
     alignItems: 'center',
     justifyContent: 'center',
   },
